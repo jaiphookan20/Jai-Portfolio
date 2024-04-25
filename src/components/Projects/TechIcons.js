d5e43a1afc53755c3f8ec5cba1d0123760825100
Jai Phookan <jaiphookan20@gmail.com>
2024-04-25 11:15:22 +1000

    made changes

--- src/components/Projects/TechIcons.js
+++ src/components/Projects/TechIcons.js
@@ -1,49 +1,60 @@
-import React from 'react';
-import { DiReact, DiFirebase,DiMongodb, DiCss3, DiJavascript, DiWindows, DiJava  } from 'react-icons/di';
-import {FaNode, FaNodeJs, FaReact} from 'react-icons/fa'
-import {FcLinux} from 'react-icons/fc'
-import { SiRedux, SiJson, SiHtml5, SiVisualstudiocode, SiJavascript, SiSpring, SiMysql, SiHibernate, SiAmazonaws, SiPostman, SiSwagger, SiExpress} from 'react-icons/si';
+import React from "react";
+import {
+  DiReact,
+  DiFirebase,
+  DiMongodb,
+  DiCss3,
+  DiJavascript,
+  DiWindows,
+  DiJava,
+  DiAndroid,
+} from "react-icons/di";
+import { FaNode, FaNodeJs, FaReact } from "react-icons/fa";
+import { FcLinux } from "react-icons/fc";
+import {
+  SiRedux,
+  SiJson,
+  SiHtml5,
+  SiVisualstudiocode,
+  SiJavascript,
+  SiSpring,
+  SiMysql,
+  SiHibernate,
+  SiAmazonaws,
+  SiPostman,
+  SiSwagger,
+  SiExpress,
+  SiKotlin,
+  SiGooglemaps,
+} from "react-icons/si";
 
-
-const TechIcons = ({tag}) => {
-    if(tag === 'Spring')
-        return <SiSpring size = '35%' color = '#559134' />
-    else if(tag === 'Firebase')
-        return <DiFirebase size = '35%' color='#F7C428'/>
-    else if(tag === 'MySQL')
-        return <SiMysql size = '35%' color = '#559134'/>
-    else if(tag === 'Hibernate')
-        return <SiHibernate size = '35%' color = '#a69e81'/>
-    else if(tag === 'CSS')
-        return <DiCss3 size = '35%'  color = "#2171B6"/>
-    else if(tag === 'JavaScript')
-        return <SiJavascript size = '35%' color = '#EFD81C'/>
-    else if(tag === 'Swagger')
-        return <SiSwagger size = '35%' color='#20fa48'/>
-    else if(tag === 'HTML')
-        return <SiHtml5 size = '35%' color = "#DC4A25"/>  
-    else if(tag === 'AWS')
-        return <SiAmazonaws size = '35%' color = '#E06C00'/> 
-    else if(tag === 'Linux')
-        return <FcLinux size = '35%'/>
-    else if(tag === 'VS code')
-        return <SiVisualstudiocode size = '35%' color = '#2174B3'/>
-    else if(tag === 'Java')
-        return <DiJava size = '35%' color = '#E06C00'/>
-    else if(tag === 'Postman')
-        return <SiPostman size = '35%' color = '#E06C00'/>
-    else if(tag === 'MongoDB')
-        return <DiMongodb size = '35%' color = '#559134'/>
-    else if(tag === 'NodeJs')
-        return <FaNodeJs size = '35%' color = '#559134'/>
-    else if(tag === 'Express')
-        return <SiExpress size = '35%' color = '#FFF'/>
-    else if(tag === 'React')
-        return <FaReact size = '35%' color = '#2171B6'/>
-    else{
-        return <FaNodeJs size = '35%' color = '#559134'/>
-    }
-    
-   
-}
+const TechIcons = ({ tag }) => {
+  if (tag === "Spring") return <SiSpring size="35%" color="#559134" />;
+  else if (tag === "Firebase") return <DiFirebase size="35%" color="#F7C428" />;
+  else if (tag === "MySQL") return <SiMysql size="35%" color="#559134" />;
+  else if (tag === "Hibernate")
+    return <SiHibernate size="35%" color="#a69e81" />;
+  else if (tag === "CSS") return <DiCss3 size="35%" color="#2171B6" />;
+  else if (tag === "JavaScript")
+    return <SiJavascript size="35%" color="#EFD81C" />;
+  else if (tag === "Swagger") return <SiSwagger size="35%" color="#20fa48" />;
+  else if (tag === "HTML") return <SiHtml5 size="35%" color="#DC4A25" />;
+  else if (tag === "AWS") return <SiAmazonaws size="35%" color="#E06C00" />;
+  else if (tag === "Linux") return <FcLinux size="35%" />;
+  else if (tag === "VS code")
+    return <SiVisualstudiocode size="35%" color="#2174B3" />;
+  else if (tag === "Java") return <DiJava size="35%" color="#E06C00" />;
+  else if (tag === "Postman") return <SiPostman size="35%" color="#E06C00" />;
+  else if (tag === "MongoDB") return <DiMongodb size="35%" color="#559134" />;
+  else if (tag === "NodeJs") return <FaNodeJs size="35%" color="#559134" />;
+  else if (tag === "Express") return <SiExpress size="35%" color="#FFF" />;
+  else if (tag === "React") return <FaReact size="35%" color="#2171B6" />;
+  else if (tag === "Kotlin") return <SiKotlin size="35%" color="#2171B6" />;
+  else if (tag === "Android") return <DiAndroid size="35%" color="#2171B6" />;
+  else if (tag === "Google Maps")
+    return <SiGooglemaps size="35%" color="#2171B6" />;
+  else {
+    return <FaNodeJs size="35%" color="#559134" />;
+  }
+};
 export default TechIcons;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,50 +1,56 @@
 export const projects = [
   {
-    title: 'Full Stack Vacation Rental Reservation Platform',
-      description: 
-      "Created a responsive full-stack web application for booking vacation rentals using MongoDB, React Express, NodeJS, Tailwind CSS and AWS S3. Numerous features added included: Registration, Login, Creation of New Listings, Booking functionality etc. Used JWT for authentication",
-      image: '/images/vacation-rental-home-page.png',
-      tags: ['MongoDB', 'React', 'Express', 'NodeJS'],
-    source: 'https://github.com/jaiphookan20/Full-Stack-Vacation-Rental-Platform',
-    visit: 'https://full-stack-vacation-rental-platform-jai.vercel.app/',
+    title: "Real-Time Location Tracking App for Families",
+    description:
+      "Created a Android application that incorporates real-time, simultaneous location tracking for multiple family members & uses geofencing technology to trigger alerts when members enter or exit designated zones",
+    image: "/images/safenest_img.png",
+    tags: ["Android", "Java", "Kotlin", "Firebase", "Google Maps"],
+    source:
+      "https://github.com/jaiphookan20/Android-Real-Time-Location-Tracking-App-for-Families/tree/main",
+    visit:
+      "https://github.com/jaiphookan20/Android-Real-Time-Location-Tracking-App-for-Families/tree/main",
     id: 0,
   },
   {
-    title: 'Plant Nursery Application',
-      description: 
-      "Created the REST API for a Plant Nursery Management System. This API performs all the fundamental CRUD operations with user validation at every step. Features include: Registration, Login, Product Search & Filtering, Add to Cart etc ",
-      image: '/images/plantnursery.jpeg',
-      tags: ['Java', 'Spring', 'MySQL', 'Postman'],
-    source: 'https://github.com/jaiphookan20/Online-Plant-Nursery-Management-System',
-    visit: 'https://drive.google.com/file/d/1eKnLrlwUok9-FAxlgeeAAoYmzrCp94zu/view?usp=sharing',
+    title: "Realtime, Collaborative Drawing Board & Chat Application",
+    description:
+      "Created a Multi-Threaded Real-Time, Collaborative Shared Drawing Board where multiple users can draw on a shared canvas together at the same time and chat together at the same time",
+    image: "/images/collaborative-drawing-board.png",
+    tags: ["Java"],
+    source:
+      "https://github.com/jaiphookan20/RealTime-Collaborative-DrawingBoard",
+    visit:
+      "https://github.com/jaiphookan20/RealTime-Collaborative-DrawingBoard",
     id: 1,
   },
   {
-    title: 'Pharmeasy',
-      description: 
-      "PharmEasy.com is India's larget e-commerce pharmacy player with a valuation of over US$5bn. Features & Pages Created Include: Landing Page, Login/Sign-Up, Search Bar, Order Medicines Page, Product Categories, Add to Cart Page etc.. ",
-      image: '/images/1.png',
-      tags: ['JavaScript', 'JSON', 'CSS'],
-    source: 'https://github.com/jaiphookan20/Pharmeasy-Clone',
-    visit: 'https://pharmeasy-clone1.vercel.app/index.html',
+    title: "Full Stack Vacation Rental Reservation Platform",
+    description:
+      "Created a responsive full-stack web application for booking vacation rentals using MongoDB, React Express, NodeJS, Tailwind CSS and AWS S3. Numerous features added included: Registration, Login, Creation of New Listings, Booking functionality etc. Used JWT for authentication",
+    image: "/images/vacation-rental-home-page.png",
+    tags: ["MongoDB", "React", "Express", "NodeJS"],
+    source:
+      "https://github.com/jaiphookan20/Full-Stack-Vacation-Rental-Platform",
+    visit: "https://full-stack-vacation-rental-platform-jai.vercel.app/",
     id: 2,
   },
   {
-    title: 'HomeToGo',
-    description:"HomeToGo is a Europe-Based marketplace with the world's largest selection of vacation rentals.              Some of the individual pages & features created from the HomeToGo portal that we cloned include the following pages: Home Page, Search Page, Listing your property, Wish List, Sign up & Login.",
-    image: '/images/2.png',
-    tags: ['JavaScript', 'JSON', 'CSS'],
-    source: 'https://github.com/jaiphookan20/HomeToGo-Clone',
-    visit: 'https://lively-biscuit-358745.netlify.app/',
+    title: "Plant Nursery Application",
+    description:
+      "Created the REST API for a Plant Nursery Management System. This API performs all the fundamental CRUD operations with user validation at every step. Features include: Registration, Login, Product Search & Filtering, Add to Cart etc ",
+    image: "/images/plantnursery.jpeg",
+    tags: ["Java", "Spring", "MySQL", "Postman"],
+    source:
+      "https://github.com/jaiphookan20/Online-Plant-Nursery-Management-System",
+    visit: "https://jaiphookan20.github.io/Swagger-API-for-Plant-Nursery-API/",
     id: 3,
   },
-  
 ];
 
 export const TimeLineData = [
-  { year: 2017, text: 'Started my journey', },
-  { year: 2018, text: 'Worked as a freelance developer', },
-  { year: 2019, text: 'Founded JavaScript Mastery', },
-  { year: 2020, text: 'Shared my projects with the world', },
-  { year: 2021, text: 'Started my own platform', },
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
 ];
